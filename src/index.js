6e76d87f50c8e2cb86955aee4710518ecd4ce8de
Ksenia C <ksenia@Ksenias-MacBook-Pro.local>
2022-09-10 12:52:34 -0400

    retreived people from DB

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, doc } from "firebase/firestore";
+import { getFirestore, collection, doc, getDocs } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -20,6 +20,8 @@ const people = [];
 document.addEventListener("DOMContentLoaded", () => {
 	//set up the dom events
 
+	getPeople();
+
 	document
 		.getElementById("btnCancelPerson")
 		.addEventListener("click", hideOverlay);
@@ -34,35 +36,47 @@ document.addEventListener("DOMContentLoaded", () => {
 	document.getElementById("btnAddIdea").addEventListener("click", showOverlay);
 });
 
-// function buildPeople(people) {
-// 	//build the HTML
-// 	let ul = document.querySelector("ul.person-list");
-// 	let months = [
-// 		"January",
-// 		"February",
-// 		"March",
-// 		"April",
-// 		"May",
-// 		"June",
-// 		"July",
-// 		"August",
-// 		"September",
-// 		"October",
-// 		"November",
-// 		"December",
-// 	];
-// 	//replace the old ul contents with the new.
-// 	ul.innerHTML = people
-// 		.map((person) => {
-// 			const dob = `${months[person["birth-month"] - 1]} ${person["birth-day"]}`;
-// 			//Use the number of the birth-month less 1 as the index for the months array
-// 			return `<li data-id="${person.id}" class="person">
-//             <p class="name">${person.name}</p>
-//             <p class="dob">${dob}</p>
-//           </li>`;
-// 		})
-// 		.join("");
-// }
+async function getPeople() {
+	const querySnapshot = await getDocs(collection(db, "people"));
+	querySnapshot.forEach((doc) => {
+		//every `doc` object has a `id` property that holds the `_id` value from Firestore.
+		//every `doc` object has a doc() method that gives you a JS object with all the properties
+		const data = doc.data();
+		const id = doc.id;
+		people.push({ id, ...data });
+	});
+	buildPeople(people);
+}
+
+function buildPeople(people) {
+	//build the HTML
+	let ul = document.querySelector("ul.person-list");
+	let months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
+	//replace the old ul contents with the new.
+	ul.innerHTML = people
+		.map((person) => {
+			const dob = `${months[person["birth-month"] - 1]} ${person["birth-day"]}`;
+			//Use the number of the birth-month less 1 as the index for the months array
+			return `<li data-id="${person.id}" class="person">
+            <p class="name">${person.name}</p>
+            <p class="dob">${dob}</p>
+          </li>`;
+		})
+		.join("");
+}
 
 function hideOverlay(ev) {
 	ev.preventDefault();
