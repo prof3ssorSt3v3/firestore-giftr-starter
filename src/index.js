b14fe43741334b1c5c7ee7faadd26615c7c5045b
Nasra <fara0197@algonquinlive.com>
2022-09-15 16:40:55 -0400

    new branch: firebase connection settings

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, doc } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyA11AtBsjU2Kr_TCGdySvB0IDt2XyfbB7E',
-  authDomain: 'stupid-first-project.firebaseapp.com',
-  projectId: 'stupid-first-project',
-  storageBucket: 'stupid-first-project.appspot.com',
-  messagingSenderId: '876050898658',
-  appId: '1:876050898658:web:d4d7add948d9a578b948bb',
+  apiKey: "AIzaSyAO23Vn0VmMWHP11CYoUWshK7bpL_iTTnI",
+  authDomain: "fire-giftr-fce59.firebaseapp.com",
+  projectId: "fire-giftr-fce59",
+  storageBucket: "fire-giftr-fce59.appspot.com",
+  messagingSenderId: "619977319882",
+  appId: "1:619977319882:web:0cc81a9f47160221a31b33",
+  measurementId: "G-11CPY45CG8"
 };
-//TODO: replace this config object with your own
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// get a reference to the database
-const db = getFirestore(app);
-
+const analytics = getAnalytics(app);
 document.addEventListener('DOMContentLoaded', () => {
   //set up the dom events
   document
