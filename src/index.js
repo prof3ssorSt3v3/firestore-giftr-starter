db43d367e4e0a8be85434165f4bb4e89115da770
Ksenia C <ksenia@Ksenias-MacBook-Pro.local>
2022-09-14 21:53:10 -0400

    added editing people

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import {
 	query,
 	addDoc,
 	onSnapshot,
+	setDoc,
 } from "firebase/firestore";
 
 // Your web app's Firebase configuration
@@ -121,29 +122,40 @@ function buildPeople(people) {
 		.map((person) => {
 			const dob = `${months[person["birth-month"] - 1]} ${person["birth-day"]}`;
 			//Use the number of the birth-month less 1 as the index for the months array
-			return `<li data-id="${person.id}" class="person">
-						<div class="person-top">
+			return `<li data-id="${person.id}" data-name="${person.name}" data-month="${person["birth-month"]}" data-day="${person["birth-day"]}" class="person">
+						<div class ="person-container">
+						<div class="person-info">
 						<p class="name">${person.name}</p>
+						<p class="dob">${dob}</p>
+						</div>
 
 						<div class="person-actions">
-						<button id="btnDeletePerson"class="person-button">
+						<button data-id="btnDeletePerson"class="person-button delete">
 						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<path d="M19 11V20.4C19 20.5591 18.9368 20.7117 18.8243 20.8243C18.7117 20.9368 18.5591 21 18.4 21H5.6C5.44087 21 5.28826 20.9368 5.17574 20.8243C5.06321 20.7117 5 20.5591 5 20.4V11M10 17V11M14 17V11M21 7H16M3 7H8M8 7V3.6C8 3.44087 8.06321 3.28826 8.17574 3.17574C8.28826 3.06321 8.44087 3 8.6 3H15.4C15.5591 3 15.7117 3.06321 15.8243 3.17574C15.9368 3.28826 16 3.44087 16 3.6V7M8 7H16" stroke="#ff0000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 						</svg>
 
 						</button>
 
-						<button id="btnEditPerson" class="person-button">
+						<button data-id="btnEditPerson" class="person-button edit">
 						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<path d="M13.02 5.828L15.85 3L20.799 7.95L17.97 10.778M13.02 5.828L3.41397 15.435C3.22642 15.6225 3.12103 15.8768 3.12097 16.142V20.678H7.65697C7.92217 20.6779 8.17648 20.5725 8.36397 20.385L17.97 10.778M13.02 5.828L17.97 10.778" stroke="#00000080" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 						</svg>
 						</button>
 						</div>
-            </div>
-            <p class="dob">${dob}</p>
+						</div>
+						
           </li>`;
 		})
 		.join("");
+
+	document.querySelectorAll(".person-button.delete").forEach((button) => {
+		button.addEventListener("click", showDeleteConfirm);
+	});
+
+	document.querySelectorAll(".person-button.edit").forEach((button) => {
+		button.addEventListener("click", showEditDialog);
+	});
 
 	//making 1st person selected by default
 	let selectedPerson = document.querySelector("li.person");
@@ -154,7 +166,7 @@ function buildPeople(people) {
 
 	getIdeas(personId);
 
-	document.querySelectorAll("li.person").forEach((item) => {
+	document.querySelectorAll(".person-info").forEach((item) => {
 		item.addEventListener("click", setActivePerson);
 	});
 }
@@ -216,12 +228,12 @@ function buildIdeas(ideas) {
 									</div>
 							
 									<div class="idea-actions">
-									<button id="btnDeleteIdea" class="idea-button delete">
+									<button data-id="btnDeleteIdea" class="idea-button delete">
 									<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 									<path d="M12.6667 7.33333V13.6C12.6667 13.7061 12.6245 13.8078 12.5495 13.8828C12.4745 13.9579 12.3728 14 12.2667 14H3.73333C3.62725 14 3.52551 13.9579 3.45049 13.8828C3.37548 13.8078 3.33333 13.7061 3.33333 13.6V7.33333M6.66667 11.3333V7.33333M9.33333 11.3333V7.33333M14 4.66667H10.6667M2 4.66667H5.33333M5.33333 4.66667V2.4C5.33333 2.29391 5.37548 2.19217 5.45049 2.11716C5.5255 2.04214 5.62725 2 5.73333 2H10.2667C10.3728 2 10.4745 2.04214 10.5495 2.11716C10.6245 2.19217 10.6667 2.29391 10.6667 2.4V4.66667M5.33333 4.66667H10.6667" stroke="#ff0000" stroke-linecap="round" stroke-linejoin="round"/>
 									</svg>
 									Delete</button>
-									<button id="btnEditIdea" class="idea-button">
+									<button data-id="btnEditIdea" class="idea-button">
 									<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 									<path d="M8.68002 3.88533L10.5667 2L13.866 5.3L11.98 7.18533M8.68002 3.88533L2.27602 10.29C2.15099 10.415 2.08073 10.5845 2.08069 10.7613V13.7853H5.10469C5.28148 13.7853 5.45103 13.715 5.57602 13.59L11.98 7.18533M8.68002 3.88533L11.98 7.18533" stroke="#00000080" stroke-linecap="round" stroke-linejoin="round"/>
 									</svg>
@@ -251,25 +263,36 @@ async function savePerson(ev) {
 		"birth-month": month,
 		"birth-day": day,
 	};
-	try {
-		const docRef = await addDoc(collection(db, "people"), person);
-		console.log("Document written with ID: ", docRef.id);
-		//1. clear the form fields
-		document.getElementById("name").value = "";
-		document.getElementById("month").value = "";
-		document.getElementById("day").value = "";
-		//2. hide the dialog and the overlay
+
+	let id = document.getElementById("btnSavePerson").getAttribute("data-id");
+	console.log(id);
+
+	if (!id) {
+		try {
+			const docRef = await addDoc(collection(db, "people"), person);
+			console.log("Document written with ID: ", docRef.id);
+			//1. clear the form fields
+			document.getElementById("name").value = "";
+			document.getElementById("month").value = "";
+			document.getElementById("day").value = "";
+			//2. hide the dialog and the overlay
+			hideOverlay();
+			//3. display a message to the user about success
+			alert(`${name} added`);
+			person.id = docRef.id;
+			//4. ADD the new HTML to the <ul> using the new object
+			getPeople();
+		} catch (err) {
+			console.error("Error adding document: ", err);
+			//do you want to stay on the dialog?
+			//display a mesage to the user about the problem
+		}
+	} else {
+		console.log("Person exists");
+		const docRef = await setDoc(doc(db, "people", id), person);
 		hideOverlay();
-		//3. display a message to the user about success
-		alert(`${name} added`);
-		person.id = docRef.id;
-		//4. ADD the new HTML to the <ul> using the new object
-		getPeople();
-	} catch (err) {
-		console.error("Error adding document: ", err);
-		//do you want to stay on the dialog?
-		//display a mesage to the user about the problem
 	}
+	getPeople();
 }
 
 async function saveIdea() {
@@ -307,10 +330,21 @@ async function saveIdea() {
 	}
 }
 
+function showDeleteConfirm(ev) {
+	confirm("Are you sure?");
+	let personId = ev.target.closest("li").getAttribute("data-id");
+	console.log(`Person deleted with id: ${personId}`);
+}
+
+function showEditDialog(ev) {
+	console.log("clicked edit");
+	showOverlay(ev);
+}
 function hideOverlay(ev) {
 	if (ev) {
 		ev.preventDefault();
 	}
+	document.getElementById("btnSavePerson").removeAttribute("data-id");
 	console.log("hid overlay");
 	document.querySelector(".overlay").classList.remove("active");
 	document
@@ -319,14 +353,32 @@ function hideOverlay(ev) {
 }
 function showOverlay(ev) {
 	ev.preventDefault();
-	document.querySelector(".overlay").classList.add("active");
-	console.log(ev.target.id);
-	if (ev.target.id === "btnAddPerson") {
+	if (ev.target.id === "btnAddPerson" || ev.target.id === "btnAddIdea") {
+		document.querySelector(".overlay").classList.add("active");
+		console.log(ev.target.id);
+		if (ev.target.id === "btnAddPerson") {
+			const id = "dlgPerson";
+			document.getElementById(id).classList.add("active");
+		} else {
+			const id = "dlgIdea";
+			document.getElementById(id).classList.add("active");
+		}
+	} else {
+		document.querySelector(".overlay").classList.add("active");
 		const id = "dlgPerson";
 		document.getElementById(id).classList.add("active");
-	} else {
-		const id = "dlgIdea";
-		document.getElementById(id).classList.add("active");
+
+		let li = ev.target.closest("li");
+		let personName = li.getAttribute("data-name");
+		let personDay = li.getAttribute("data-day");
+		let personMonth = li.getAttribute("data-month");
+		let personId = li.getAttribute("data-id");
+
+		document.getElementById("name").value = personName;
+		document.getElementById("day").value = personDay;
+		document.getElementById("month").value = personMonth;
+		document.getElementById("btnSavePerson").setAttribute("data-id", personId);
 	}
+
 	//TODO: check that person is selected before adding an idea
 }
