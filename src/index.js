a71ad2e48674c72a6f3b0592125e2892ba03adcf
JasreetK <90123505+JasreetK@users.noreply.github.com>
2022-09-17 18:53:44 -0400

    Handle the selected person

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, doc, getDocs } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDkx8fhcF6ONfR0JfeWpsxG1dY4SlnYeVg",
@@ -15,9 +22,9 @@ const app = initializeApp(firebaseConfig);
 // get a reference to the database
 const db = getFirestore(app);
 const people = []; //to hold all the people from the collection
+let selectedPersonId = null;
 
 document.addEventListener("DOMContentLoaded", () => {
-  //set up the dom events
   document
     .getElementById("btnCancelPerson")
     .addEventListener("click", hideOverlay);
@@ -30,6 +37,10 @@ document.addEventListener("DOMContentLoaded", () => {
     .getElementById("btnAddPerson")
     .addEventListener("click", showOverlay);
   document.getElementById("btnAddIdea").addEventListener("click", showOverlay);
+
+  document
+    .querySelector(".person-list")
+    .addEventListener("click", handleSelectPerson);
 
   loadInitialData();
 });
@@ -54,12 +65,8 @@ function loadInitialData() {
 }
 
 async function getPeople() {
-  //call this from DOMContentLoaded init function
-  //the db variable is the one created by the getFirestore(app) call.
   const querySnapshot = await getDocs(collection(db, "people"));
   querySnapshot.forEach((doc) => {
-    //every `doc` object has a `id` property that holds the `_id` value from Firestore.
-    //every `doc` object has a doc() method that gives you a JS object with all the properties
     const data = doc.data();
     const id = doc.id;
     people.push({ id, ...data });
@@ -68,7 +75,6 @@ async function getPeople() {
 }
 
 function buildPeople(people) {
-  //build the HTML
   let ul = document.querySelector("ul.person-list");
   let months = [
     "January",
@@ -84,15 +90,44 @@ function buildPeople(people) {
     "November",
     "December",
   ];
-  //replace the old ul contents with the new.
   ul.innerHTML = people
     .map((person) => {
       const dob = `${months[person["birth-month"] - 1]} ${person["birth-day"]}`;
-      //Use the number of the birth-month less 1 as the index for the months array
       return `<li data-id="${person.id}" class="person">
             <p class="name">${person.name}</p>
             <p class="dob">${dob}</p>
           </li>`;
     })
     .join("");
+
+  selectedPersonId = people[0].id;
+
+  let li = document.querySelector(`[data-id="${selectedPersonId}"]`);
+  li.click();
+}
+
+function handleSelectPerson(ev) {
+  const li = ev.target.closest(".person");
+  console.log(`${li.getAttribute("data-id")} was clicked`);
+  const id = li ? li.getAttribute("data-id") : null;
+  if (id) {
+    selectedPersonId = id;
+    if (ev.target.classList.contains("edit")) {
+      //EDIT the doc using the id to get a docRef
+      //show the dialog form to EDIT the doc (same form as ADD)
+      //Load all the Person document details into the form from docRef
+    } else if (ev.target.classList.contains("delete")) {
+      //DELETE the doc using the id to get a docRef
+      //do a confirmation before deleting
+    } else {
+      document.querySelector("li.selected")?.classList.remove("selected");
+      li.classList.add("selected");
+      //and load all the gift idea documents for that person
+      // getIdeas(id);
+    }
+  } else {
+    //clicked a button not inside <li class="person">
+    //Show the dialog form to ADD the doc (same form as EDIT)
+    //showOverlay function can be called from here or with the click listener in DOMContentLoaded, not both
+  }
 }
