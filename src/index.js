9bc8568e0302b850b72ae72541d22a1701552023
Ksenia C <ksenia@Ksenias-MacBook-Pro.local>
2022-09-21 20:17:58 -0400

    changed styling

--- src/index.js
+++ src/index.js
@@ -59,6 +59,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	document
 		.getElementById("btnAddPerson")
 		.addEventListener("click", showOverlay);
+
 	document.getElementById("btnAddIdea").addEventListener("click", showOverlay);
 
 	document
@@ -99,7 +100,6 @@ const peopleChange = onSnapshot(
 //Building people
 function buildPeople(people) {
 	let ul = document.querySelector("ul.person-list");
-	console.log("build people");
 	if (!people.length == 0) {
 		//replace the old ul contents with the new.
 		ul.innerHTML = people
@@ -115,14 +115,14 @@ function buildPeople(people) {
 						<p class="dob">${dob}</p>
 						</div>
 						<div class="person-actions">
-						<button data-id="btnDeletePerson"class="person-button delete">
-						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d="M19 11V20.4C19 20.5591 18.9368 20.7117 18.8243 20.8243C18.7117 20.9368 18.5591 21 18.4 21H5.6C5.44087 21 5.28826 20.9368 5.17574 20.8243C5.06321 20.7117 5 20.5591 5 20.4V11M10 17V11M14 17V11M21 7H16M3 7H8M8 7V3.6C8 3.44087 8.06321 3.28826 8.17574 3.17574C8.28826 3.06321 8.44087 3 8.6 3H15.4C15.5591 3 15.7117 3.06321 15.8243 3.17574C15.9368 3.28826 16 3.44087 16 3.6V7M8 7H16" stroke="#ff0000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-						</svg>
-						</button>
 						<button data-id="btnEditPerson" class="person-button edit">
 						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<path d="M13.02 5.828L15.85 3L20.799 7.95L17.97 10.778M13.02 5.828L3.41397 15.435C3.22642 15.6225 3.12103 15.8768 3.12097 16.142V20.678H7.65697C7.92217 20.6779 8.17648 20.5725 8.36397 20.385L17.97 10.778M13.02 5.828L17.97 10.778" stroke="#00000080" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+						</svg>
+						</button>
+						<button data-id="btnDeletePerson"class="person-button delete">
+						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M19 11V20.4C19 20.5591 18.9368 20.7117 18.8243 20.8243C18.7117 20.9368 18.5591 21 18.4 21H5.6C5.44087 21 5.28826 20.9368 5.17574 20.8243C5.06321 20.7117 5 20.5591 5 20.4V11M10 17V11M14 17V11M21 7H16M3 7H8M8 7V3.6C8 3.44087 8.06321 3.28826 8.17574 3.17574C8.28826 3.06321 8.44087 3 8.6 3H15.4C15.5591 3 15.7117 3.06321 15.8243 3.17574C15.9368 3.28826 16 3.44087 16 3.6V7M8 7H16" stroke="#ff0000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 						</svg>
 						</button>
 						</div>
@@ -153,7 +153,7 @@ function buildPeople(people) {
 		ul.innerHTML = `
 		<div class="no-items">
 		<p>
-		Looks like there's no people added yet. Click "Add person".
+		Looks like there's no people added yet. Click "Add person" 🤓.
 		</p>
 		</div>
 		`;
@@ -270,16 +270,19 @@ function buildIdeas(filteredIdeas) {
 									</div>
 							
 									<div class="idea-actions">
-									<button data-id="btnDeleteIdea" class="idea-button delete">
-									<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<path d="M12.6667 7.33333V13.6C12.6667 13.7061 12.6245 13.8078 12.5495 13.8828C12.4745 13.9579 12.3728 14 12.2667 14H3.73333C3.62725 14 3.52551 13.9579 3.45049 13.8828C3.37548 13.8078 3.33333 13.7061 3.33333 13.6V7.33333M6.66667 11.3333V7.33333M9.33333 11.3333V7.33333M14 4.66667H10.6667M2 4.66667H5.33333M5.33333 4.66667V2.4C5.33333 2.29391 5.37548 2.19217 5.45049 2.11716C5.5255 2.04214 5.62725 2 5.73333 2H10.2667C10.3728 2 10.4745 2.04214 10.5495 2.11716C10.6245 2.19217 10.6667 2.29391 10.6667 2.4V4.66667M5.33333 4.66667H10.6667" stroke="#ff0000" stroke-linecap="round" stroke-linejoin="round"/>
-									</svg>
-									Delete</button>
+									
+									
 									<button data-id="btnEditIdea" class="idea-button edit">
-									<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<path d="M8.68002 3.88533L10.5667 2L13.866 5.3L11.98 7.18533M8.68002 3.88533L2.27602 10.29C2.15099 10.415 2.08073 10.5845 2.08069 10.7613V13.7853H5.10469C5.28148 13.7853 5.45103 13.715 5.57602 13.59L11.98 7.18533M8.68002 3.88533L11.98 7.18533" stroke="#00000080" stroke-linecap="round" stroke-linejoin="round"/>
-									</svg>
-									Edit</button>
+									<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M13.02 5.828L15.85 3L20.799 7.95L17.97 10.778M13.02 5.828L3.41397 15.435C3.22642 15.6225 3.12103 15.8768 3.12097 16.142V20.678H7.65697C7.92217 20.6779 8.17648 20.5725 8.36397 20.385L17.97 10.778M13.02 5.828L17.97 10.778" stroke="#00000080" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+						</svg>
+						</button>
+						<button data-id="btnDeleteIdea" class="idea-button delete">
+									<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M19 11V20.4C19 20.5591 18.9368 20.7117 18.8243 20.8243C18.7117 20.9368 18.5591 21 18.4 21H5.6C5.44087 21 5.28826 20.9368 5.17574 20.8243C5.06321 20.7117 5 20.5591 5 20.4V11M10 17V11M14 17V11M21 7H16M3 7H8M8 7V3.6C8 3.44087 8.06321 3.28826 8.17574 3.17574C8.28826 3.06321 8.44087 3 8.6 3H15.4C15.5591 3 15.7117 3.06321 15.8243 3.17574C15.9368 3.28826 16 3.44087 16 3.6V7M8 7H16" stroke="#ff0000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+						</svg>
+						</button>
+									
 									</div>
 								</div>
 	        </li>`;
@@ -308,10 +311,10 @@ function buildIdeas(filteredIdeas) {
 				checkbox.addEventListener("click", markBought);
 			});
 		} else {
-			let ul = document.querySelector("ul.idea-list");
-			ul.innerHTML = `<li class="idea"> 
-						<p class="title" >No ideas for this person</p>
-	        </li>`;
+			ul.innerHTML = `
+			<p>No ideas for this person. Try adding some above 🤓
+			</p>
+			`;
 		}
 	} else {
 		// if there's no people
@@ -385,23 +388,21 @@ async function saveIdea() {
 //↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓//
 
 function deleteConfirm(ev) {
-	//If deleting a person
-	if (ev.target.getAttribute("data-id") === "btnDeleteIdea") {
+	let id = ev.target.closest("li").getAttribute("data-id");
+	console.log(id);
+	//If deleting an idea
+	if (ev.target.closest("button").getAttribute("data-id") === "btnDeleteIdea") {
 		let ideaName = ev.target.closest("li").getAttribute("data-idea");
-		let ideaId = ev.target.closest("li").getAttribute("data-id");
-		let collectionName = "gift-ideas";
 
 		if (confirm(`Are you sure you want to delete ${ideaName}?`)) {
-			deleteItem(collectionName, ideaId);
+			deleteItem("gift-ideas", id);
 		}
 	} else {
-		//If deleting an idea
+		//If deleting a person
 		let personName = ev.target.closest("li").getAttribute("data-name");
-		let personId = ev.target.closest("li").getAttribute("data-id");
-		let collectionName = "people";
 
 		if (confirm(`Are you sure you want to delete ${personName}?`)) {
-			deleteItem(collectionName, personId);
+			deleteItem("people", id);
 		}
 	}
 }
@@ -423,6 +424,7 @@ async function deleteItem(collectionName, id) {
 		querySnapshot.forEach((item) => {
 			deleteDoc(doc(db, "gift-ideas", item.id));
 		});
+		console.log(selectedPersonId);
 	}
 }
 
@@ -463,7 +465,7 @@ function showOverlay(ev) {
 			document.getElementById(id).classList.add("active");
 		}
 	} else {
-		if (ev.target.getAttribute("data-id") === "btnEditIdea") {
+		if (ev.target.closest("button").getAttribute("data-id") === "btnEditIdea") {
 			const id = "dlgIdea";
 			document.getElementById(id).classList.add("active");
 			let li = ev.target.closest("li");
