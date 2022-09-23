5e4981136d9926cdecb4461c35a9ff6251275b93
Leo Batan <leobatan@Leos-MacBook-Pro.local>
2022-09-23 13:10:43 -0400

    Define constants and add domcontentloaded event listeners

--- src/index.js
+++ src/index.js
@@ -23,3 +23,64 @@ const firebaseConfig = {
   messagingSenderId: "314280737166",
   appId: "1:314280737166:web:533bf1bc8e7b438770ccdf",
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const people = [];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let personId = "";
+let ideaId = "";
+document.addEventListener("DOMContentLoaded", () => {
+  getPeople();
+  document
+    .getElementById("btnAddPerson")
+    .addEventListener("click", showOverlay);
+  document.getElementById("btnAddIdea").addEventListener("click", showOverlay);
+  document
+    .getElementById("btnCancelPerson")
+    .addEventListener("click", hideOverlay);
+  document
+    .getElementById("btnCancelIdea")
+    .addEventListener("click", hideOverlay);
+  document.querySelector(".overlay").addEventListener("click", hideOverlay);
+  document
+    .getElementById("btnSavePerson")
+    .addEventListener("click", savePerson);
+  document.getElementById("btnSaveIdea").addEventListener("click", saveIdea);
+  document
+    .querySelector(".person-list")
+    .addEventListener("click", handleSelectPerson);
+  document
+    .querySelector(".idea-list")
+    .addEventListener("click", handleSelectIdea);
+  document.getElementById("btnYes").addEventListener("click", deletePerson);
+  document.getElementById("btnYesDelete").addEventListener("click", deleteIdea);
+
+  onSnapshot(collection(db, "people"), (snapshot) => {
+    let people = [];
+    snapshot.docs.forEach((doc) => {
+      people.push({ id: doc.id, ...doc.data() });
+    });
+    buildPeople(people);
+  });
+  onSnapshot(collection(db, "gift-ideas"), (snapshot) => {
+    let ideas = [];
+    snapshot.docs.forEach((doc) => {
+      ideas.push({ id: doc.id, ...doc.data() });
+    });
+    buildIdeas(ideas);
+  });
+});
