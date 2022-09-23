1f653b4d777116009e8da27edc6c810af67b66dd
Leo Batan <leobatan@Leos-MacBook-Pro.local>
2022-09-23 13:09:59 -0400

    Add firebase to webpack proj

--- src/index.js
+++ src/index.js
@@ -1,48 +1,25 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, doc } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  doc,
+  getDocs,
+  getDoc,
+  addDoc,
+  setDoc,
+  orderBy,
+  onSnapshot,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA11AtBsjU2Kr_TCGdySvB0IDt2XyfbB7E',
-  authDomain: 'stupid-first-project.firebaseapp.com',
-  projectId: 'stupid-first-project',
-  storageBucket: 'stupid-first-project.appspot.com',
-  messagingSenderId: '876050898658',
-  appId: '1:876050898658:web:d4d7add948d9a578b948bb',
+  apiKey: "AIzaSyDgLEo4-GeXRZkLPXJ40Hvns6Gd-8qdi18",
+  authDomain: "fire-giftr-1b0b0.firebaseapp.com",
+  projectId: "fire-giftr-1b0b0",
+  storageBucket: "fire-giftr-1b0b0.appspot.com",
+  messagingSenderId: "314280737166",
+  appId: "1:314280737166:web:533bf1bc8e7b438770ccdf",
 };
-//TODO: replace this config object with your own
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// get a reference to the database
-const db = getFirestore(app);
-
-document.addEventListener('DOMContentLoaded', () => {
-  //set up the dom events
-  document
-    .getElementById('btnCancelPerson')
-    .addEventListener('click', hideOverlay);
-  document
-    .getElementById('btnCancelIdea')
-    .addEventListener('click', hideOverlay);
-  document.querySelector('.overlay').addEventListener('click', hideOverlay);
-
-  document
-    .getElementById('btnAddPerson')
-    .addEventListener('click', showOverlay);
-  document.getElementById('btnAddIdea').addEventListener('click', showOverlay);
-});
-
-function hideOverlay(ev) {
-  ev.preventDefault();
-  document.querySelector('.overlay').classList.remove('active');
-  document
-    .querySelectorAll('.overlay dialog')
-    .forEach((dialog) => dialog.classList.remove('active'));
-}
-function showOverlay(ev) {
-  ev.preventDefault();
-  document.querySelector('.overlay').classList.add('active');
-  const id = ev.target.id === 'btnAddPerson' ? 'dlgPerson' : 'dlgIdea';
-  //TODO: check that person is selected before adding an idea
-  document.getElementById(id).classList.add('active');
-}
